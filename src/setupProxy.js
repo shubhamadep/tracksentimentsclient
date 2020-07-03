9e2b3d3a72a14b2998e2506efe37a602b2340e98
Shubham <shubham.adep@wsu.edu>
2020-07-02 22:23:57 -0700

    Added URL in route to test PRODUCTION backend API call

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,6 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-    app.use('/getproductdetails', createProxyMiddleware({ target : 'https://amazonsellerproductinfo.herokuapp.com', changeOrigin: true}));
-    app.use(createProxyMiddleware('/reviews/scrape', { target : 'https://amazonsellerproductinfo.herokuapp.com' }));
-}
+// module.exports = function(app) {
+//     app.use('/getproductdetails', createProxyMiddleware({ target : 'https://amazonsellerproductinfo.herokuapp.com', changeOrigin: true}));
+//     app.use(createProxyMiddleware('/reviews/scrape', { target : 'https://amazonsellerproductinfo.herokuapp.com' }));
+// }
--- src/util/product.js
+++ src/util/product.js
@@ -4,7 +4,7 @@ export function getProductDetails(value) {
     console.log("Calling API...........")
     return new Promise((resolve, reject) => {
       axios
-        .get('/getproductdetails/scrape?SellerID='+value)
+        .get('https://amazonsellerproductinfo.herokuapp.com/getproductdetails/scrape?SellerID='+value)
         .then(
           res => resolve(res.data),
           err => reject(err)
