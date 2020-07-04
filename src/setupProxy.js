81601c9e55197c2326fc580f26ad9803bd0c0a6e
Shubham <shubham.adep@wsu.edu>
2020-07-03 17:43:50 -0700

    removed http-proxy-middleware due to production issues

--- src/setupProxy.js
+++
@@ -1,6 +0,0 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware');
-
-// module.exports = function(app) {
-//     app.use('/getproductdetails', createProxyMiddleware({ target : 'https://amazonsellerproductinfo.herokuapp.com', changeOrigin: true}));
-//     app.use(createProxyMiddleware('/reviews/scrape', { target : 'https://amazonsellerproductinfo.herokuapp.com' }));
-// }
